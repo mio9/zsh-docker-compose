be1484530f99531f45e4857e20d94867c8ed1785
MiO9 <admin@mio9.sh>
2025-09-18 01:05:26 +0900

    Update zsh-docker-compose.plugin.zsh

--- zsh-docker-compose.plugin.zsh
+++ zsh-docker-compose.plugin.zsh
@@ -4,6 +4,12 @@ updown ()
   docker compose down && docker compose up -d
 }
 
+updownl ()
+{
+  docker compose down && docker compose up -d && docker compose logs -f "$1"
+}
+
 alias dcdown='docker compose down'
 alias dcup='docker compose up -d'
 alias dcex='docker compose exec'
+alias dclog='docker compose logs -f'
